ffc05ccb36596310fa596a096ae7c4d3e2add8f8
samequemarques0 <sameque12@gmail.com>
2026-09-06 13:25:17 -0300

    body transparente

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const { app, BrowserWindow } = require('electron');
 function createWindow() {
   const win = new BrowserWindow({
     width: 380,
-    height: 220,
+    height: 180,
     resizable: false,
+    transparent: true,
     frame: false, // Opcional: remove as bordas padrão do Windows para um visual limpo
     webPreferences: {
       nodeIntegration: true,
